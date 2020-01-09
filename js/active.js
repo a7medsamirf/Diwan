250a5edb60a905e3ebcab583ad38638ce5d2930c
ahmed samir <ahmed.samir.farid@gmail.com>
2020-01-09 17:06:02 +0200

    Update active.js

--- js/active.js
+++ js/active.js
@@ -1,10 +1,3 @@
-/* =====================================
-Template Name: TravelTrek
-Author Name: iThemer
-Author URI: http://ithemer.com/
-Description: TravelTrek is a Travel Agency & Tour Booking HTML5 Template.
-Version:	1.0
-========================================*/
 /*=======================================
 [Start Activation Code]
 =========================================
@@ -313,3 +306,22 @@ Version:	1.0
 			});
 		});
 })(jQuery);
+
+
+
+(function () {
+var options = {
+facebook: "", // Facebook page ID
+whatsapp: "+966508638445", // WhatsApp number
+call_to_action: "Message us", // Call to action
+button_color: "#FF6550", // Color of button
+position: "left", // Position may be 'right' or 'left'
+order: "facebook,whatsapp", // Order of buttons
+};
+var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
+
+
